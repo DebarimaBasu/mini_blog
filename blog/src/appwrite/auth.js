7488fb24aa4af76d2221e213daeea3e6163ac260
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-02-02 13:14:58 +0530

    creating auth.js

---
+++ blog/src/appwrite/auth.js
@@ -0,0 +1,62 @@
+import conf from "../conf/conf"
+import { Account, Client, Databases, ID } from "appwrite";
+
+export class AuthService{
+    client=new Client;//when object will be created the client will also be created
+    account;
+    constructor()
+    {
+        this.client
+        .setEndpoint(appwriteUrl)
+        .setProject(appwriteProjectId);
+        this.account=new Account(client);
+    }
+
+
+async createAccount({email,password,name})
+{
+    try{
+       const userAccount= await this.account.create(ID.unique(),email,password,name);
+       if(userAccount){
+            return this.logIn({email,password});//if already sign up then returning log in
+       }
+       else{
+        return userAccount
+       }
+    }
+    catch(error){
+        throw error;
+    }
+}
+async logIn({email,password})
+{
+    try{
+      return  await this.account.createEmailPasswordSession(email,password);
+    }
+    catch (error)
+    { throw error
+    }
+}
+async getCurrentUser()
+{
+    try{
+        return await this.account.get();//returning current state of user
+    }
+    catch(error)
+    {
+        console.log("Appwrite service::getCurrentUser::error",error);
+    }
+    return null;//if error it will return null
+}
+async logOut()
+{
+try{
+    return await this.account.deleteSessions();
+}
+catch{
+    throw error
+}
+}
+}
+const authService=new AuthService
+export default authService
