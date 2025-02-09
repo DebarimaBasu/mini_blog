dc856305892c21053076b1bbf4b25f629d88a78c
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-02-09 12:17:31 +0530

    creating RTE

--- blog/src/components/AuthLayout.jsx
+++ blog/src/components/AuthLayout.jsx
@@ -2,23 +2,24 @@ import React,{useEffect,useState} from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 
-export default function AuthLayout  ({children,authentication=true})  {
+export default function Protected ({children,authentication=true})  {
     const navigate=useNavigate();
     const [loader,setLoader]=useState(true);
     const authStatus=useSelector(state=>state.auth.status);
     useEffect(()=>{
-        if(authStatus!==authentication){
-            navigate("/");
+      //true && false!==true 
+      //when authentication is true and authStatus is false then navigate to login page
+      //authentication will be true beacause we send the default value as true if authsatatus is false
+      //it means user is not logged in so we need to navigate to login page
+        if( authentication && authStatus!==authentication){
+            navigate("/login");
         }
-        else if(authStatus==="failed"){
-            setLoader(false);
+        else if(!authentication && authStatus===authentication){
+           navigate("/");
         }
+        setLoader(false);
     },[authStatus,navigate,authentication]);
-  return (
-    <div>
-      
-    </div>
-  )
+  return loader ? <h1>loading...</h1> : <>{children}</>
 }
 
 
